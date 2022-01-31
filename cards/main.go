79e539c8471d0c90982b6408b8290d13c3ae3d7b
aim <ahmadhakim404@gmail.com>
2022-01-31 21:53:19 +0700

    deklarasi variabel pada go

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var card string = "Full house"
+	card := "Full House"
+	// card := "ace of spades" -> ini akan error
+	card = "ace of spades"
+	fmt.Println(card)
 }
