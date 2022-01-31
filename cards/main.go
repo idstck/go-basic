fc94b5b0559b2fe3f6b7e9534f18faa7f548b205
aim <ahmadhakim404@gmail.com>
2022-02-01 00:13:48 +0700

    fungsi dan nilai balik (return)

--- cards/main.go
+++ cards/main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	// var card string = "Full house"
-	card := "Full House"
+	// card := "Full House"
 	// card := "ace of spades" -> ini akan error
-	card = "ace of spades"
+	// card = "ace of spades"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Lima Hati"
 }
