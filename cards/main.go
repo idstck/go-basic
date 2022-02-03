befe5d17658dc9cd901cc69a60f09ec652fcb036
IDStack <idstack@outlook.co.id>
2022-02-03 17:45:22 +0700

    array dan perulangan di golang

--- cards/main.go
+++ cards/main.go
@@ -7,8 +7,14 @@ func main() {
 	// card := "Full House"
 	// card := "ace of spades" -> ini akan error
 	// card = "ace of spades"
-	card := newCard()
-	fmt.Println(card)
+	// card := newCard()
+	// fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Straight")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
